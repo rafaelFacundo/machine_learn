b6847c4daa64c42a3d486a46eca607bb6bc4c8c9
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-03-13 21:44:48 -0300

    gradient descent done - need to test and plot graphics

--- linear_regression/gradient_descent.py
+++ linear_regression/gradient_descent.py
@@ -2,11 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #reading data from the dataset
-data_from_artificial1d = np.genfromtxt("./artificial1d.csv", delimiter=',')
+data_from_artificial1d = np.genfromtxt(u"/home/rafaelfacundo/Documents/machine_learn/linear_regression/artificial1d.csv", delimiter=',')
 
 alpha  = float(input("Type the learning rate: ")); # alpha is the learning rate of our model
-epochs = int(input("Type the number of iterations(epochs): ")) 
-W_zero = 0;
-W_one  = 0;
-t      = 0;
+epochs = int(input("Type the number of iterations(epochs): ")) # number of iterations
+w_zero = 0; # parameter of the model
+w_one  = 0; # parameter of the model
+column_x_from_data = data_from_artificial1d[:,0]; # taking column x, the column of the inputs
+column_y_from_data = data_from_artificial1d[:,1]; # taking column y, the column of the output values for each input from x
+N = column_x_from_data.shape[0];
+for t in range(epochs):    
+    y_hat = w_zero + w_one * column_x_from_data;
+    e_i   = column_y_from_data - y_hat;
+    w_zero = w_zero + alpha * (np.sum(e_i) / N);
+    e_i_times_column_x = e_i * column_x_from_data;
+    w_one  = w_one + alpha * (np.sum(e_i_times_column_x) / N)
 
+print("w0: ", w_zero);
+print("W1: ", w_one)
