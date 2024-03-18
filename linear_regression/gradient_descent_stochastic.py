880f6aa6f05777080296d326738aece4cb951b03
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-03-18 20:03:45 -0300

    gradient descent stochastic done

--- linear_regression/gradient_descent_stochastic.py
+++ linear_regression/gradient_descent_stochastic.py
@@ -14,7 +14,7 @@ N = column_x_from_data.shape[0];
 for t in range(epochs):
     column_x_from_data_exchanged = np.random.permutation(data_from_artificial1d);
     column_x_from_data = data_from_artificial1d[:,0]; # taking column x, the column of the inputs
-    column_y_from_data = data_from_artificial1d[:,1]; # taking column y, the column of the output values for each input from x
+    column_y_from_data = data_from_artificial1d[:,1]; # tak0ing column y, the column of the output values for each input from x
     for i in range(N):
         y_hat  = w_zero + w_one * column_x_from_data[i];
         e_i    = column_y_from_data[i] - y_hat;
