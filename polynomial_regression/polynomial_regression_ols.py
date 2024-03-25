456d664e5f772f418ead9149caa04db716afc86d
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-03-25 20:16:54 -0300

    min max normalization done but need to test

--- polynomial_regression/polynomial_regression_ols.py
+++ polynomial_regression/polynomial_regression_ols.py
@@ -18,6 +18,18 @@ def create_x_matrice(dataMatrice, degree):
         X = np.hstack((X, newColumnsRaisedToPowerP));
     return X
 
+def normalization_minMax(xMatrice):
+    X = np.ones((xMatrice.shape[0], 1));
+    for col in range(xMatrice.shape[1]):
+        column = xMatrice[:,col];
+        maximum = column.max()
+        minimum = column.min()
+        column = column + minimum
+        column = column * (1/ (maximum - minimum))
+        X = np.hstack((X, column.reshape((column.shape[0], 1))))
+    return X
+
+
 # now I gonna take 80% of the data to make the train set
 # I will choice each data in an aleatory way
 testSetSize = int(0.2 * data_from_california.shape[0])  
@@ -28,10 +40,14 @@ trainSet = np.delete(data_from_california, testIndices, axis=0)
 inputValues = trainSet[:, 0:8];
 Yvalues = trainSet[:,8];
 
-print(inputValues)
-print("=====================")
+inputNormalized = normalization_minMax(inputValues)
 
-X = create_x_matrice(trainSet, 2)
+print(inputValues);
+print("+++++++++")
+print(inputNormalized);
+print("+++++++++")
+
+X = create_x_matrice(inputNormalized, 3)
 print(X)
 
 X_transpose = np.transpose(X)
